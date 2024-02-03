c1d1431e948327558d163b0668702bb02bb5b859
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-02-03 14:23:53 +0000

    Started interpreter (pg 57)

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # lox-csharp-interpreter
+
+An implemetation of an interpreter for the lox language from the book Crafting Interpreters
+
+## Sources
+
+System exit codes can be found here:
+
+https://man.openbsd.org/sysexits
+
---
+++ lox/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program {
+    private static void Main(string[] args) {
+        if (args.Length == 1) RunFile(args[0]);
+        else if (args.Length == 0) RunPrompt();
+        else {
+            Console.WriteLine("Usage: lox [script]");
+        }
+
+        static void RunFile(string file) {
+            string text = File.ReadAllText(file);
+            Run(text);
+        }
+
+        static void RunPrompt() {
+            while (true) {
+                Console.Write(">>> ");
+                string? line = Console.ReadLine();
+                if (line is null) {
+                    Console.WriteLine();
+                    break;
+                }
+                Run(line);
+            }
+        }
+
+        static void Run(string source) {
+            Scanner scanner = new(source);
+    }
+}
